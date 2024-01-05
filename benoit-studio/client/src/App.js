84a52753d6d3db24c9cf82dfc55d9827862bb40a
bonfire203 <46801720+bonfire203@users.noreply.github.com>
2024-01-04 19:55:17 -0700

    navigation fix

--- benoit-studio/client/src/App.js
+++ benoit-studio/client/src/App.js
@@ -10,8 +10,8 @@ function App() {
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/work" element={<Work />} />
-            <Route path="/about" element={<About />} />
+            <Route path="work" element={<Work />} />
+            <Route path="about" element={<About />} />
         </Routes>
     </BrowserRouter>
   );
--- benoit-studio/client/src/components/work.js
+++ benoit-studio/client/src/components/work.js
@@ -1,7 +1,9 @@
+import Header from './header';
 const Work = () => {
     return (
+        
         <div className='main-container defaultpage'>
-
+            <Header/>
 
             <div className='border-container'>
                 <h1>Work Page</h1>
